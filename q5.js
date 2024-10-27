29ac78399a86f52f9112aaf8a2badea81c0a1eb7
Caê <mcmlins17@gmail.com>
2024-10-27 17:50:53 -0300

    questão 4 refeita

--- q5.js
+++ q5.js
@@ -3,19 +3,19 @@ const prompt=require("prompt-sync")({sigint:true});
 let valor1 = parseFloat(prompt(`Digite um valor: `));
 let valor2 = parseFloat(prompt(`Digite outro valor: `));
 
-let cod = parseFloat(prompt(`digite a operação que deseja realizar: 1-media\n2-diferenca\n3-produto entre os numeros\n4-divisao do primeiro pelo segundo`));
+let cod = parseFloat(prompt(`digite a operação que deseja realizar:\n1-media\n2-diferenca\n3-produto entre os numeros\n4-divisao do primeiro pelo segundo\n>>> `));
 
 switch (cod){
     case 1:
-        console.log(`alimento nao perecivel`);
+        console.log(`${(valor1+valor2)/2}`);
         break;
     case 2:
-        console.log(`alimento nao perecivel`);
+        console.log(`${valor1-valor2}`);
         break;
     case 3:
-        console.log(`alimento nao perecivel`);
+        console.log(`${valor1*valor2}`);
         break;
     case 4:
-        console.log(`alimento perecivel`)
+        console.log(`${valor1/valor2}`)
         break;
 }
